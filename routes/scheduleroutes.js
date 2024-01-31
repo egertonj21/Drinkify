3c97d6ffead9f7e7206b15de61a482c7b83d7351
Joel Egerton <egertonjoel@gmail.com>
2024-01-31 20:40:50 +0000

    setup basic server

--- routes/scheduleroutes.js
+++ routes/scheduleroutes.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const conn = require('../utils/dbconn');
-const router = express.Router();
-router.get('/', (req, res) => {
 
-const selectSQL = 'SELECT * FROM runschedule';
-conn.query(selectSQL, (err, rows) => {
-if (err) {
-throw err;
-} else {
-res.render('index', { schedule: rows });
-}
-});
-});
+const router = express.Router();
+
+
+
 
 module.exports = router;
